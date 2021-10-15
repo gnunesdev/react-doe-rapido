a0c55d6915fc81afda04f75b8cf12096403b7abc
Carlos Henrique <carlos.henrique0307200@gmail.com>
2021-10-14 22:51:26 -0300

    feat(edit-company): ajustar layout do container

--- src/pages/edit-company/components/EditCompanyShell/styles.ts
+++ src/pages/edit-company/components/EditCompanyShell/styles.ts
@@ -4,9 +4,9 @@ import { InputContainer } from '~/components/Input/styles';
 import { SelectContainer } from '~/components/Select/styles';
 
 export const Container = styled.div`
+  flex: 1;
   padding: 48px 64px;
   max-width: 1140px;
-  width: 100%;
 `;
 
 export const Form = styled.form`
