e0315b17d225fd4f8ef0daae997ac11138150934
Carlos Henrique <carlos.henrique0307200@gmail.com>
2021-10-28 23:07:42 -0300

    feat(header): ajustar header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,21 +2,42 @@ import styled from 'styled-components';
 
 export const HeaderContainer = styled.header`
   width: 100%;
-  height: 6.2rem;
+  height: 62px;
+  padding: 0 24px;
   background: ${(props) => props.theme.colors.primary};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
+export const Logo = styled.div`
   display: flex;
   align-items: center;
+`;
 
-  padding: 0 2.4rem;
+export const Title = styled.h1`
+  ${(props) => props.theme.typography.headline}
+  color: ${(props) => props.theme.foreground.primary};
+`;
 
-  h1 {
-    font-size: 2.8rem;
-    font-weight: bold;
-    display: block;
+export const Icon = styled.div`
+  margin-left: 8px;
+`;
 
-    color: ${(props) => props.theme.colors.white};
+export const UserArea = styled.div`
+  display: flex;
+  align-items: center;
+`;
 
-    margin-right: 0.8rem;
-  }
+export const UserName = styled.div`
+  ${(props) => props.theme.typography.body2}
+  color: ${(props) => props.theme.foreground.primary};
+`;
+
+export const UserIcon = styled.div`
+  margin-left: 24px;
+  width: 42px;
+  height: 42px;
+  border-radius: 50%;
+  background-color: orange;
 `;
