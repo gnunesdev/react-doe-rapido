3c58543bcdfd354f7011b1cdd758b69fa2be3ebd
Carlos Henrique <carlos.henrique0307200@gmail.com>
2021-10-28 22:52:48 -0300

    feat(general): Ajustar tema

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -6,16 +6,19 @@ declare module 'styled-components' {
     colors: {
       primary: string;
       secondary: string;
-
       background: string;
-
-      white: string;
-      black: string;
       red: string;
     };
-
+    foreground: {
+      primary: string;
+    };
     containers: {
       desktop: string;
+    };
+    typography: {
+      headline: string;
+      title: string;
+      body: string;
     };
   }
 }
--- src/styles/themes/light.ts
+++ src/styles/themes/light.ts
@@ -1,17 +1,33 @@
-export default {
+import { DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
   title: 'light',
   colors: {
     primary: '#3483B4',
     secondary: '#62b6cb',
-
     background: '#F3F3F3',
-
-    white: '#FFF',
-    black: '#000',
     red: '#ff4d4d',
   },
-
+  foreground: {
+    primary: '#FFF',
+  },
   containers: {
     desktop: 'calc(100vh - 6.2rem)',
   },
+  typography: {
+    headline: `
+      font-size: 2.4rem;
+      font-weight: 900;
+    `,
+    title: `
+      font-size: 3.6rem;
+      font-weight: 900;
+    `,
+    body: `
+      font-size: 18px;
+      font-weight: 900;
+    `,
+  },
 };
+
+export default theme;
